7b976b59dca63b1b44dcb04b4c09ceb5f8c7afb1
Sai Subham Das <saisubham2701@gmail.com>
2023-06-14 00:00:04 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def registration(request):
+    return render(request,'registration.html')
+
+def login(request):
+    return render(request,'login.html')
+    
+def extra(request):
+    return render(request,'extra.html')  
